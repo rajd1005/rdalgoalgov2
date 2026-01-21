e5e8ef24a5646db7d55ae814effd4e27ded67d52
rajd1005 <rajdey.dey1@gmail.com>
2026-01-21 09:30:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+import json
 
 db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False) # Store Hashed Passwords
+    is_admin = db.Column(db.Boolean, default=False)
+    
+    # Subscription Management
+    subscription_end = db.Column(db.DateTime, nullable=True)
+    is_trial = db.Column(db.Boolean, default=False)
+    
+    # Zerodha Credentials (Encrypted/Stored as JSON)
+    zerodha_creds = db.Column(db.Text, nullable=True) 
+    
+    def set_creds(self, api_key, api_secret, totp, uid, pwd):
+        data = {
+            "api_key": api_key,
+            "api_secret": api_secret,
+            "totp": totp,
+            "user_id": uid,
+            "password": pwd
+        }
+        self.zerodha_creds = json.dumps(data)
+        
+    def get_creds(self):
+        if not self.zerodha_creds: return None
+        return json.loads(self.zerodha_creds)
+
+    @property
+    def is_active_sub(self):
+        """Check if subscription is valid"""
+        if self.is_admin: return True
+        return self.subscription_end and self.subscription_end > datetime.now()
 
 class AppSetting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
